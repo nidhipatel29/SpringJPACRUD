38f48134bef67d59cf9c98163942c2643aa1e36e
Nidhi <nidhi2910patel@gmail.com>
2026-07-11 09:59:45 -0400

    implemented find ,update,delete methods from DAO

--- src/main/java/com/example/JPA_CRUD/DAO/EmployeeDaoImpl.java
+++ src/main/java/com/example/JPA_CRUD/DAO/EmployeeDaoImpl.java
@@ -2,6 +2,7 @@ package com.example.JPA_CRUD.DAO;
 
 import java.util.List;
 
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
@@ -15,26 +16,43 @@ import jakarta.transaction.Transactional;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
 
-    private EntityManager entityManager;
+  private EntityManager entityManager;
 
-    @Autowired
-     public EmployeeDaoImpl(EntityManager entityManager){
-       this.entityManager=entityManager;
-     }
+  @Autowired
+  public EmployeeDaoImpl(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
 
-    @Transactional
-    public void save(Employee theEmployee){
-      entityManager.persist(theEmployee);
-    }
+  @Transactional
+  public void save(Employee theEmployee) {
+    entityManager.persist(theEmployee);
+  }
 
-    public Employee findById(int id){
-       return entityManager.find(Employee.class, id);
-   
-    }
+  public Employee findById(int id) {
+    return entityManager.find(Employee.class, id);
 
-    public List<Employee> readAllEmployee(){
-       TypedQuery<Employee> query=entityManager.createQuery("FROM Employee",Employee.class);
-      return query.getResultList();
-    }
-    
+  }
+
+  public List<Employee> readAllEmployee() {
+    TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
+    return query.getResultList();
+  }
+
+  public List<Employee> findByPara(String department) {
+    TypedQuery<Employee> query = entityManager.createQuery("FROM Employee WHERE department=:thedata", Employee.class);
+    query.setParameter("thedata", department);
+    return query.getResultList();
+  }
+
+  @Transactional
+  public void updateEmployee(Employee employee){
+     entityManager.merge(employee);
+  }
+
+  @Transactional
+  public int deleteEmployee(int id){
+    Query query=entityManager.createQuery("DELETE FROM Employee WHERE id=:id");
+    query.setParameter("id", id);
+    return query.executeUpdate();
+  }
 }
